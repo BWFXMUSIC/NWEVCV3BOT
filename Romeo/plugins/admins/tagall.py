fb8eed80a153f1e2d7027cda1042dddf39aed471
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-01-22 22:02:32 +0530

    Update tagall.py

--- Romeo/plugins/admins/tagall.py
+++ Romeo/plugins/admins/tagall.py
@@ -6,51 +6,61 @@ from pyrogram import filters
 SPAM_CHATS = []
 
 
-@app.on_message(filters.command(["tagall", "all"]) | filters.command("@all", "") & filters.group)
-async def tag_all_users(_,message): 
-    replied = message.reply_to_message  
-    if len(message.command) < 2 and not replied:
-        await message.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ**") 
-        return                  
-    if replied:
-        SPAM_CHATS.append(message.chat.id)      
-        usernum= 0
-        usertxt = ""
-        async for m in app.get_chat_members(message.chat.id): 
-            if message.chat.id not in SPAM_CHATS:
-                break       
-            usernum += 5
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 1:
-                await replied.reply_text(usertxt)
-                await asyncio.sleep(2)
-                usernum = 0
-                usertxt = ""
-        try :
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass
+@app.on_message(filters.command(["tagall", "all", "tagmember"], prefixes=["/", "@", "#"]))
+async def mentionall(client, message):
+    chat_id = message.chat.id
+    if message.chat.type == "private":
+        return await message.reply("This command can be used in groups and channels!")
+
+    is_admin = False
+    try:
+        participant = await client.get_chat_member(chat_id, message.from_user.id)
+    except UserNotParticipant:
+        is_admin = False
     else:
-        text = message.text.split(None, 1)[1]
-        
-        SPAM_CHATS.append(message.chat.id)
-        usernum= 0
-        usertxt = ""
-        async for m in app.get_chat_members(message.chat.id):       
-            if message.chat.id not in SPAM_CHATS:
-                break 
-            usernum += 1
-            usertxt += f"\n💌 [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 5:
-                await app.send_message(message.chat.id,f'{text}\n{usertxt}')
-                await asyncio.sleep(2)
-                usernum = 0
-                usertxt = ""                          
-        try :
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass        
-           
+        if participant.status in ("administrator", "creator"):
+            is_admin = True
+    if not is_admin:
+        return await message.reply("Only admin can use this command!")
+
+    if message.reply_to_message and message.text:
+        return await message.reply("/tagall hello 👈 Try this next time for tagging..")
+    elif message.text:
+        mode = "text_on_cmd"
+        msg = message.text
+    elif message.reply_to_message:
+        mode = "text_on_reply"
+        msg = message.reply_to_message
+        if not msg:
+            return await message.reply("/tagall hii 👈 Try this or reply any message...")
+    else:
+        return await message.reply("/tagall hii 👈 Try this or reply any message...")
+
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_chat_members(chat_id):
+        if not chat_id in spam_chats:
+            break
+        if usr.user.is_bot:
+            continue
+        usrnum += 1
+        usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}) "
+
+        if usrnum == 1:
+            if mode == "text_on_cmd":
+                txt = f"{usrtxt} {random.choice(TAGMES)}"
+                await client.send_message(chat_id, txt)
+            elif mode == "text_on_reply":
+                await msg.reply(f"[{random.choice(EMOJI)}](tg://user?id={usr.user.id})")
+            await asyncio.sleep(2)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
+
 @app.on_message(filters.command(["cancel", "stop"]))
 async def cancel_spam(client, message):
     if not message.chat.id in spam_chats:
